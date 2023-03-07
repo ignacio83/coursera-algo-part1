18e56ea4593acd1a25887cafa1ecd19468449b9f
André de Fontana Ignacio <ignacio83@gmail.com>
2023-03-07 10:09:48 -0300

    feat: HelloGoodbye exercise.

--- HelloWorld/src/main/java/HelloGoodbye.java
+++ HelloWorld/src/main/java/HelloGoodbye.java
@@ -1,32 +1,6 @@
 public class HelloGoodbye {
   public static void main(String[] args) {
-    sayHello(args);
-    sayGoodbye(args);
-  }
-
-  private static void sayHello(String[] args) {
-    final var sbHello = new StringBuilder("Hello ");
-    for (int i = 0; i < args.length; i++) {
-      sbHello.append(args[i]);
-      if (i + 1 != args.length) {
-        sbHello.append(" and ");
-      } else {
-        sbHello.append(".");
-      }
-    }
-    System.out.println(sbHello);
-  }
-
-  private static void sayGoodbye(String[] args) {
-    final var sbGoodbye = new StringBuilder("Goodbye ");
-    for (int i = args.length - 1; i > -1; i--) {
-      sbGoodbye.append(args[i]);
-      if (i != 0) {
-        sbGoodbye.append(" and ");
-      } else {
-        sbGoodbye.append(".");
-      }
-    }
-    System.out.println(sbGoodbye);
+    System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+    System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
   }
 }
--- HelloWorld/src/test/java/HelloGoodbyeTest.java
+++ HelloWorld/src/test/java/HelloGoodbyeTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class HelloGoodbyeTest {
 
   @Test
-  public void TesTwoNames() {
+  public void Test() {
     var out = SystemStream.getOutput();
 
     HelloGoodbye.main(new String[] {"Kevin", "Bob"});
@@ -14,19 +14,5 @@ public class HelloGoodbyeTest {
 				Hello Kevin and Bob.
 				Goodbye Bob and Kevin.
 				""", out.toString());
-  }
-
-  @Test
-  public void TesThreeNames() {
-    var out = SystemStream.getOutput();
-
-    HelloGoodbye.main(new String[] {"Kevin", "Bob", "John"});
-
-    assertEquals(
-        """
-				Hello Kevin and Bob and John.
-				Goodbye John and Bob and Kevin.
-				""",
-        out.toString());
   }
 }
