92c5d74421e94a33e1dc797a133570404713f2de
André de Fontana Ignacio <andre.ignacio@unico.io>
2023-03-27 17:24:03 -0300

    feat: Changes to Java 8 level.

--- HelloWorld/src/main/java/RandomWord.java
+++ HelloWorld/src/main/java/RandomWord.java
@@ -5,9 +5,9 @@ import edu.princeton.cs.algs4.StdRandom;
 public class RandomWord {
   public static void main(String[] args) {
     String champion = null;
-    for (var i = 0; !StdIn.isEmpty(); i++) {
-      final var word = StdIn.readString();
-      final var ok = StdRandom.bernoulli(1 / (i + 1.0));
+    for (int i = 0; !StdIn.isEmpty(); i++) {
+      final String word = StdIn.readString();
+      final boolean ok = StdRandom.bernoulli(1 / (i + 1.0));
       if (ok) {
         champion = word;
       }
